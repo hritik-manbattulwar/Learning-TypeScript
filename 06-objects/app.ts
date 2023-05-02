a920a019b9acf798afebedebd6ee3b1652d6249e
hritik-manbattulwar <hritik.m@newput.com>
2023-05-02 11:07:02 +0530

    06-objects

---
+++ 06-objects/app.ts
@@ -0,0 +1,46 @@
+// Object -> a real world entity
+interface Mobile {
+    brand : string,
+    color : string,
+    price : number,
+    isInsured : boolean
+}
+
+let mobile:Mobile = {
+    brand : 'Apple',
+    color : 'Silver',
+    price : 35000,
+    isInsured : false
+};
+console.log(mobile);
+console.log(mobile.color);
+
+// Nested Object
+interface Address {
+    street : string,
+    city : string,
+    state : string
+}
+
+interface Student {
+    name : string,
+    age : number,
+    course : string,
+    college : string,
+    address : Address
+}
+
+let student:Student = {
+    name : 'Arjun Reddy',
+    age : 23,
+    course : "MBBS",
+    college : "Osmania Medical College",
+    address : {
+        street : "Anand Nagar , Jubilee Hills",
+        city : "Hyderabad",
+        state : "TS"
+    }
+};
+console.log(student);
+console.log(student.address);
+
